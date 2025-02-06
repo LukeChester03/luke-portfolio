f7517d66a5e9167a61cdfd2789f63c32e07afc83
Luke Chester <lukechester03@outlook.com>
2025-02-06 15:18:09 +0000

    fixed positioning on contact page

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -14,10 +14,7 @@ import {
 } from "@/components/ui/Select";
 import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt } from "react-icons/fa";
 import { motion } from "framer-motion";
-const info = [
-  { icon: <FaPhoneAlt />, title: "Phone", description: "(+44) 7718 646 377" },
-  { icon: <FaEnvelope />, title: "Email", description: "lukechester03@outlook.com" },
-];
+const info = [{ icon: <FaEnvelope />, title: "Email", description: "lukechester03@outlook.com" }];
 const Contact = () => {
   return (
     <motion.section
@@ -26,8 +23,27 @@ const Contact = () => {
       className="py-6"
     >
       <div className="container mx-auto">
-        <div className="flex flex-col xl:flex-row gap-[30px]">
-          <div className="xl:h-[54%] order-2 xl:order-none ">
+        <div className="flex flex-col xl:flex-col gap-[30px]">
+          {/* info */}
+          <div className="flex-1 flex items-start xl:justify-start  mb-8 xl:mb-0">
+            <ul className="flex flex-col gap-10">
+              {info.map((item, index) => {
+                return (
+                  <li key={index} className="flex items-center gap-6">
+                    <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
+                      <div className="text-[28px]">{item.icon}</div>
+                    </div>
+                    <div>
+                      <p className="text-white/60">{item.title}</p>
+                      <h3 className="text-xl">{item.description}</h3>
+                    </div>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+          {/* form */}
+          <div className="xl:h-[54%]  ">
             <form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
               <h3 className="text-4xl text-accent ">Let's get in touch</h3>
               <p className="text-white/60">
@@ -36,7 +52,7 @@ const Contact = () => {
                 message!
               </p>
               {/* input */}
-              <div className="grid frid-cols-1 md:grid-cols-2 gap-6 ">
+              <div className="grid grid-cols-1 md:grid-cols-2 gap-6 ">
                 <Input type="firstname" placeholder="First Name" />
                 <Input type="lastname" placeholder="Last Name" />
                 <Input type="email" placeholder="Email" />
@@ -59,28 +75,10 @@ const Contact = () => {
                 </SelectContent>
               </Select>
               <Textarea className="h-[200px]" placeholder="Type your message here..." />
-              <Button size={"md"} className="max-w-40 self-center items-center justify-center">
+              <Button size={"lg"} className="max-w-60 self-center items-center justify-center">
                 Send Message
               </Button>
             </form>
-          </div>
-          {/* info */}
-          <div className="flex-1 flex items-center xl:justify-end order-1 xl:order-none mb-8 xl:mb-0">
-            <ul className="flex flex-col gap-10">
-              {info.map((item, index) => {
-                return (
-                  <li key={index} className="flex items-center gap-6">
-                    <div className="w-[52px] h-[52px] xl:w-[72px] xl:h-[72px] bg-[#27272c] text-accent rounded-md flex items-center justify-center">
-                      <div className="text-[28px]">{item.icon}</div>
-                    </div>
-                    <div>
-                      <p className="text-white/60">{item.title}</p>
-                      <h3 className="text-xl">{item.description}</h3>
-                    </div>
-                  </li>
-                );
-              })}
-            </ul>
           </div>
         </div>
       </div>
