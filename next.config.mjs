78eb1cbcaad92a92b233ce68afdd6e7583d0df8a
Luke Chester <lukechester03@outlook.com>
2025-07-06 20:16:10 +0100

    fix

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,7 @@ import path from "path";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack(config) {
-    config.resolve.alias["@"] = path.resolve("./");
+    config.resolve.alias["@"] = path.resolve(process.cwd(), ".");
     return config;
   },
 };
